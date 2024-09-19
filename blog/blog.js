1de384d1c348348dba078e8e94d61a30bc7d02fa
Lalit Patil <lalitpatil@outlook.com>
2024-09-20 01:22:13 +0530

    added posts text, metadata, fixed scroll

--- blog/blog.js
+++ blog/blog.js
@@ -13,6 +13,15 @@ let images = [
     "assets/5.jpg",
     "assets/6.jpg",
     "assets/7.jpg",
+    "assets/8.jpg",
+    "assets/9.jpg",
+    "assets/10.jpg",
+    "assets/11.jpg",
+    "assets/12.jpg",
+    "assets/13.jpg",
+    "assets/14.jpg",
+    "assets/15.jpg",
+    "assets/16.jpg",
 ];
 
 images.forEach((img, idx) => {
@@ -20,6 +29,9 @@ images.forEach((img, idx) => {
     image.src = img;
     image.draggable = false;
     image.className = "img";
+    image.dataset.postTitle = `title${idx+1}`;
+    image.dataset.postText = `text${idx+1}`;
+    image.dataset.postDate = `date${idx+1}`;
     image.id = `assets/${idx+1}.jpg`;
     track.appendChild(image);
 })
@@ -31,13 +43,14 @@ function transform(section) {
         const scrollSection = section.querySelector('.scroll-section');
         let percentage = (((window.scrollY) - offsetTop) / window.innerHeight)*100;
         //percentage *= 0.3;
-        percentage = percentage < 0 ? 0 : percentage>400 ? 400 : percentage;
+        console.log(percentage);
+        percentage = percentage < 0 ? 0 : percentage>300 ? 300 : percentage;
         scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`;
 
-        console.log(percentage);
+        // console.log(percentage);
         for(const card of track.getElementsByClassName("img")) {
             card.animate({
-                objectPosition : `${percentage}%  -50%`
+                objectPosition : `${percentage/2}%  -50%`
             }, {duration:1200, fill: "forwards"})
         }
     }
--- blog/on-click-post.js
+++ blog/on-click-post.js
@@ -3,15 +3,18 @@ const stickyContainer = document.getElementById("sticky-container");
 const fullPage = document.querySelector('#fullpage');
 const blogPostContainer = document.querySelector('#blog-post-container');
 const backButton = document.getElementById("back-button");
+const blogPostTitleContainer = document.getElementById("blog-post-title-container");
+const blogPostDateContainer = document.getElementById("blog-post-date-container");
+const blogPostTextContainer = document.getElementById("blog-post-text-container");
 
 posts.forEach(function(post) {
     post.addEventListener("click", function(e) {
         let selImage = images.find(image => image === post.id);
-        transformPostsTrack(selImage);
+        transformPostsTrack(selImage, post);
     });
 });
 
-function transformPostsTrack(selImage) {
+function transformPostsTrack(selImage, post) {
     console.log(selImage);
 
     horizontalScroll = false;
@@ -19,6 +22,9 @@ function transformPostsTrack(selImage) {
     stickyContainer.style.display = "none";
 
     blogPostContainer.style.backgroundImage = 'url(' + selImage + ')';
+    blogPostTitleContainer.textContent = eval(post.dataset.postTitle);
+    blogPostDateContainer.textContent = eval(post.dataset.postDate);
+    blogPostTextContainer.textContent = eval(post.dataset.postText);
     fullPage.style.display = 'flex';
 
     backButton.style.display = 'block';
@@ -31,3 +37,28 @@ backButton.addEventListener("click", function() {
     backButton.style.display = 'none';
     horizontalScroll = true;
 });
+
+
+// function getTitleForPost(postId) {
+//     return eval(postId);
+//     // switch(postId){
+//     //     case 'title1':
+//     //         return title1;
+//     //     case 'title2':
+//     //         return title2;
+//     //     default:
+//     //         return 'no title found';
+//     // }
+// }
+
+// function getTextForPost(postId) {
+//     return eval(postId);
+//     // switch(postId){
+//     //     case 'text1':
+//     //         return text1;
+//     //     case 'text2':
+//     //         return text2;
+//     //     default:
+//     //         return 'no text found';
+//     // }
+// }
